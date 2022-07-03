52bb444a927550d0fdea429d54f80b0c973733f2
hoangphong <phong.pnhoang@gmail.com>
2022-07-03 15:04:08 +0700

    fix item

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -9,7 +9,7 @@ function AccountItem() {
     return (
         <div className={cx('wrapper')}>
             <img
-                className={cx('avataraaaaaaa')}
+                className={cx('avatar')}
                 src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_300x300.webp?x-expires=1651489200&x-signature=Xp8iH14MDegegBpFEg%2Fg5rv6Mrw%3D"
                 alt="Hoaa"
             />
